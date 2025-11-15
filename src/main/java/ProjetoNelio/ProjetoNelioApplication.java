2001ee29699c7d06128ed6388af2241cdda34456
Pedro Vitor Nunes Silva <pedrovitornunes89@gmail.com>
2025-11-15 10:10:34 -0300

    Criando o projeto

---
+++ src/main/java/ProjetoNelio/ProjetoNelioApplication.java
@@ -0,0 +1,13 @@
+package ProjetoNelio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoNelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoNelioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ProjetoNelio/ProjetoNelioApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjetoNelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoNelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
