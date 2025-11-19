63778f8d51bf262ddd9c21049888aa97a3bad182
Pedro Vitor Nunes Silva <pedrovitornunes89@gmail.com>
2025-11-19 18:09:38 -0300

    implementando a associacao de products e orders

--- src/main/java/ProjetoNelio/entities/Product.java
+++ src/main/java/ProjetoNelio/entities/Product.java
@@ -1,11 +1,13 @@
 package ProjetoNelio.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "tb_product")
@@ -24,6 +26,9 @@ public class Product implements Serializable {
     joinColumns = @JoinColumn(name = "product_id"),
     inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> orders = new HashSet<>();
 
     public Product() {
     }
@@ -78,6 +83,12 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> set =  orders.stream().map(OrderItem::getOrder).collect(Collectors.toSet());
+        return set;
     }
 
     @Override
